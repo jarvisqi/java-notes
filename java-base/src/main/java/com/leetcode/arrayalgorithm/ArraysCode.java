f88575af11748b90f9ea6339a0494fa74693cdcf
jarvisqi <qdw8911@sina.com>
2018-07-25 16:04:06 +0800

    update

--- java-base/src/main/java/com/leetcode/arrayalgorithm/ArraysCode.java
+++ java-base/src/main/java/com/leetcode/arrayalgorithm/ArraysCode.java
@@ -19,7 +19,6 @@ public class ArraysCode {
         int[] arrs = {1, 2, 3, 4, 5, 6, 7};
         rotate(arrs, 3);
 
-
     }
 
     /**
@@ -80,49 +79,76 @@ public class ArraysCode {
      */
     public static void rotate(int[] nums, int k) {
 
-        int length = nums.length;
-        if (length <= 0) {
-            return;
-        }
-        int[] newArrs = new int[length];
-        for (int i = 0; i < length; i++) {
-            newArrs[i] = nums[i];
-        }
-        k = k % length;
-        int t;
-        for (int i = 0; i < length; i++) {
-            t = (i + k) % length;
-            nums[t] = newArrs[i];
-        }
+//        int length = nums.length;
+//        if (length <= 0) {
+//            return;
+//        }
+//        int[] newArrs = new int[length];
+//        for (int i = 0; i < length; i++) {
+//            newArrs[i] = nums[i];
+//        }
+//        k = k % length;
+//        int t;
+//        for (int i = 0; i < length; i++) {
+//            t = (i + k) % length;
+//            nums[t] = newArrs[i];
+//        }
         /*
          *以上是最佳方案
          *以下方案会在数据很多超时
          */
+//
+//        int len = nums.length;
+//        if (len <= 0) {
+//            return;
+//        }
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < len; i++) {
+//            list.add(nums[i]);
+//        }
+//        System.out.println(list);
+//
+//        for (int i = 0; i < k; i++) {
+//            //最后一个值
+//            int index = list.get(len - 1);
+//            //后面的顺序排列
+//            List<Integer> newList = list.stream().filter(x -> x != index).collect(Collectors.toList());
+//            newList.add(0, index);
+//            list = newList;
+//        }
 
+
+        //以下方法在本地对，leetcode 上不正确
         int len = nums.length;
-        if (len == 0 || len == 1 || k == 0) {
+        if (len <= 0) {
             return;
         }
+
+        int arrLen = nums.length;
+        if (arrLen <= 0) {
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            int[] newArr = new int[arrLen];
+            newArr[0] = nums[arrLen - 1];
+            for (int j = 0; j < arrLen; j++) {
+                if ((j + 1) < arrLen) {
+                    newArr[j + 1] = nums[j];
+                }
+            }
+            nums = newArr;
+        }
+
+//        for (int i = 0; i < list.size(); i++) {
+//            nums[i] = list.get(i);
+//        }
+
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < len; i++) {
             list.add(nums[i]);
         }
         System.out.println(list);
-
-        for (int i = 0; i < k; i++) {
-            //最后一个值
-            int index = list.get(len - 1);
-            //后面的顺序排列
-            List<Integer> newList = list.stream().filter(x -> x != index).collect(Collectors.toList());
-            newList.add(0, index);
-            list = newList;
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            nums[i] = list.get(i);
-        }
-
-        System.out.println(list);
     }
+
 }
 
