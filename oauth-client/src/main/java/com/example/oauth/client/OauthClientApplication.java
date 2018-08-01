f74621b5335e906e0744af12ef7cbe18a0227a72
jarvisqi <qdw8911@sina.com>
2018-08-01 23:03:01 +0800

    oauth-client

---
+++ oauth-client/src/main/java/com/example/oauth/client/OauthClientApplication.java
@@ -0,0 +1,18 @@
+package com.example.oauth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * OAuth2.0 client
+ *
+ * @author Jarvis
+ * @date 2018/08/01
+ */
+@SpringBootApplication
+public class OauthClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthClientApplication.class, args);
+    }
+}
