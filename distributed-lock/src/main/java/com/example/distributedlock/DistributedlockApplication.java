3591484289126d088795992b9a1e0644bbc31754
jarvisqi <qdw8911@sina.com>
2018-08-01 18:55:02 +0800

    redis 分布式锁

---
+++ distributed-lock/src/main/java/com/example/distributedlock/DistributedlockApplication.java
@@ -0,0 +1,18 @@
+package com.example.distributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 分布式锁
+ *
+ * @author Jarvis
+ * @date 2018/08/01
+ */
+@SpringBootApplication
+public class DistributedlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedlockApplication.class, args);
+    }
+}
---
+++ distributed-lock/src/test/java/com/example/distributedlock/DistributedlockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.distributedlock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DistributedlockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
