d3b341811b8b67e10570ce34e044f6007779e036
jarvisqi <qdw8911@sina.com>
2018-08-07 11:14:34 +0800

    spring boot + rocketmq

---
+++ rockct-tutorial/src/main/java/com/mq/rockct/tutorial/RockctTutorialApplication.java
@@ -0,0 +1,19 @@
+package com.mq.rockct.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * rocketMQ
+ *
+ * @author Jarvis
+ * @date 2018/08/06
+ */
+@SpringBootApplication
+public class RockctTutorialApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RockctTutorialApplication.class, args);
+    }
+}
