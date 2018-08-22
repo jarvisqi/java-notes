09e5a4fe98bb0d79c3d913bdf3502198703a9e77
jarvisqi <qdw8911@sina.com>
2018-08-22 16:38:22 +0800

    aop

---
+++ base-code/src/main/java/com/basecode/aop/UserBeanImpl.java
@@ -0,0 +1,47 @@
+package com.basecode.aop;
+
+/**
+ * @author Jarvis
+ * @date 2018/8/22
+ */
+public class UserBeanImpl implements UserBean {
+    private String user = null;
+
+    public UserBeanImpl() {
+    }
+
+    public UserBeanImpl(String user) {
+        this.user = user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+        System.out.println("this is setUser() method!");
+    }
+
+    public String getUserName() {
+        return user;
+    }
+
+    @Override
+    public void getUser() {
+        System.out.println("this is getUser() method!");
+    }
+
+    @Override
+    public void addUser() {
+        System.out.println("this is addUser() method!");
+    }
+
+    @Override
+    public void updateUser() {
+        System.out.println("this is updateUser() method!");
+    }
+
+    @Override
+    public void deleteUser() {
+        System.out.println("this is deleteUser() method!");
+    }
+
+
+}
