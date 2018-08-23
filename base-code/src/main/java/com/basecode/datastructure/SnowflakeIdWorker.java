e8a5c41e7e209690d6073d573089483075c00a6d
jarvisqi <qdw8911@sina.com>
2018-08-23 11:35:46 +0800

    分布式自增ID算法snowflake

--- base-code/src/main/java/com/basecode/datastructure/SnowflakeIdWorker.java
+++ base-code/src/main/java/com/basecode/datastructure/SnowflakeIdWorker.java
@@ -2,9 +2,11 @@ package com.basecode.datastructure;
 
 import cn.hutool.core.date.DateTime;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Twitter的分布式自增ID算法snowflake (Java版)
- * SnowFlake的结构如下(每部分用-分开):  0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ * SnowFlake的二进制结构(每部分用-分开):  0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
  * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
  * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
  * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
@@ -12,6 +14,8 @@ import cn.hutool.core.date.DateTime;
  * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
  * 加起来刚好64位，为一个Long型。<br>
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
+ * 单台机器实例，通过时间戳保证前41位是唯一的，分布式系统多台机器实例下，通过对每个机器实例分配不同的datacenterId和workerId避免中间的10位碰撞。
+ * 最后12位每毫秒从0递增生产ID，再提一次：每毫秒最多生成4096个ID，每秒可达4096000个。
  *
  * @author Jarvis
  * @date 2018/8/22
@@ -38,7 +42,7 @@ public class SnowflakeIdWorker {
      */
     private final long maxDatacenterId = ~(-1L << datacenterIdBits);
     /**
-     * 序列在id中占的位数
+     * 序列在id中占的位数，自增序列位数
      */
     private final long sequenceBits = 12L;
     /**
@@ -54,7 +58,7 @@ public class SnowflakeIdWorker {
      */
     private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
     /**
-     * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+     * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) 自增序列最大生成数
      */
     private final long sequenceMask = ~(-1L << sequenceBits);
 
@@ -67,7 +71,7 @@ public class SnowflakeIdWorker {
      */
     private long datacenterId;
     /**
-     * 毫秒内序列(0~4095)
+     * 毫秒内序列(0~4095) 自增序列值
      */
     private long sequence = 0L;
     /**
@@ -154,9 +158,30 @@ public class SnowflakeIdWorker {
 class SnowflakeTest {
 
     public static void main(String[] args) {
+        //获取指定时间戳
+        long timestamp = DateTime.of("2018-01-01", "yyyy-MM-dd").getTime();
+        System.out.println(timestamp);
+        //时间戳转时间
+        DateTime dt = new DateTime(timestamp);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String text = dateFormat.format(dt);
+
+
+        System.out.println(dt);
+        System.out.println(text);
+
+        // 运行时数据workerId＝1，datacenterId＝2，分别表示机器实例的生产者编号，数据中心编号；
+        //sequence＝0表示每毫秒生产ID从0开始计数递增；
         SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             long id = idWorker.nextId();
+            System.out.println(Long.toBinaryString(id));
+            System.out.println(id);
+        }
+
+        SnowflakeIdWorker idWorker2 = new SnowflakeIdWorker(1, 1);
+        for (int i = 0; i < 5; i++) {
+            long id = idWorker2.nextId();
             System.out.println(Long.toBinaryString(id));
             System.out.println(id);
         }
