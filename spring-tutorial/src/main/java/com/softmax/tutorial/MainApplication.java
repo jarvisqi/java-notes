56881eb4ec1a9d381944bc6aeda57ff62a88eef1
jarvisqi <qdw8911@sina.com>
2018-08-28 18:48:58 +0800

    update

--- spring-tutorial/src/main/java/com/softmax/tutorial/MainApplication.java
+++ spring-tutorial/src/main/java/com/softmax/tutorial/MainApplication.java
@@ -3,6 +3,12 @@ package com.softmax.tutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring boot 2.0  教程
+ *
+ * @author Jarvis
+ * @date 2018/08/28
+ */
 @SpringBootApplication
 public class MainApplication {
 
