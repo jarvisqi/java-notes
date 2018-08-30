dcda1775d772fd5692052544323f15219a95c043
jarvisqi <qdw8911@sina.com>
2018-08-30 19:10:46 +0800

    oauth2.0

---
+++ spring-tutorial/src/main/java/com/softmax/tutorial/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.softmax.tutorial.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Security Configuration
+ *
+ * @author Jarvis
+ * @date 2018/8/30
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        AuthenticationManager manager = super.authenticationManagerBean();
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .formLogin().and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
+}
