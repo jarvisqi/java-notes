018e95cc08d4ba65baf3a23a0ea78fc0824e65d6
jarvisqi <qdw8911@sina.com>
2018-09-07 18:22:52 +0800

    update auth.sql

--- oauth2-server/src/main/resources/init-auth.sql
+++ oauth2-server/src/main/resources/init-auth.sql
@@ -1,69 +1,105 @@
-CREATE DATABASE IF NOT EXISTS oauth2db;
-USE oauth2db;
+USE `oauth2db` ;
 
-CREATE TABLE IF NOT EXISTS oauth_client_details (
-  client_id               VARCHAR(256) PRIMARY KEY,
-  resource_ids            VARCHAR(256),
-  client_secret           VARCHAR(256),
-  scope                   VARCHAR(256),
-  authorized_grant_types  VARCHAR(256),
-  web_server_redirect_uri VARCHAR(256),
-  authorities             VARCHAR(256),
-  access_token_validity   INTEGER,
-  refresh_token_validity  INTEGER,
-  additional_information  VARCHAR(4096),
-  autoapprove             VARCHAR(256)
-);
+-- -----------------------------------------------------
+-- Table `oauth2db`.`clientdetails`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`clientdetails` (
+  `appId` VARCHAR(128) NOT NULL,
+  `resourceIds` VARCHAR(256) NULL DEFAULT NULL,
+  `appSecret` VARCHAR(256) NULL DEFAULT NULL,
+  `scope` VARCHAR(256) NULL DEFAULT NULL,
+  `grantTypes` VARCHAR(256) NULL DEFAULT NULL,
+  `redirectUrl` VARCHAR(256) NULL DEFAULT NULL,
+  `authorities` VARCHAR(256) NULL DEFAULT NULL,
+  `access_token_validity` INT(11) NULL DEFAULT NULL,
+  `refresh_token_validity` INT(11) NULL DEFAULT NULL,
+  `additionalInformation` VARCHAR(4096) NULL DEFAULT NULL,
+  `autoApproveScopes` VARCHAR(256) NULL DEFAULT NULL,
+  PRIMARY KEY (`appId`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS oauth_client_token (
-  token_id          VARCHAR(256),
-  token             BLOB,
-  authentication_id VARCHAR(256) PRIMARY KEY,
-  user_name         VARCHAR(256),
-  client_id         VARCHAR(256)
-);
 
-CREATE TABLE IF NOT EXISTS oauth_access_token (
-  token_id          VARCHAR(256),
-  token             BLOB,
-  authentication_id VARCHAR(256) PRIMARY KEY,
-  user_name         VARCHAR(256),
-  client_id         VARCHAR(256),
-  authentication    BLOB,
-  refresh_token     VARCHAR(256)
-);
+-- -----------------------------------------------------
+-- Table `oatuh2`.`oauth_access_token`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`oauth_access_token` (
+  `token_id` VARCHAR(256) NULL DEFAULT NULL,
+  `token` BLOB NULL DEFAULT NULL,
+  `authentication_id` VARCHAR(128) NOT NULL,
+  `user_name` VARCHAR(256) NULL DEFAULT NULL,
+  `client_id` VARCHAR(256) NULL DEFAULT NULL,
+  `authentication` BLOB NULL DEFAULT NULL,
+  `refresh_token` VARCHAR(256) NULL DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS oauth_refresh_token (
-  token_id       VARCHAR(256),
-  token          BLOB,
-  authentication BLOB
-);
 
-CREATE TABLE IF NOT EXISTS oauth_code (
-  code           VARCHAR(256),
-  authentication BLOB
-);
+-- -----------------------------------------------------
+-- Table `oatuh2`.`oauth_approvals`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`oauth_approvals` (
+  `userId` VARCHAR(256) NULL DEFAULT NULL,
+  `clientId` VARCHAR(256) NULL DEFAULT NULL,
+  `scope` VARCHAR(256) NULL DEFAULT NULL,
+  `status` VARCHAR(10) NULL DEFAULT NULL,
+  `expiresAt` DATETIME NULL DEFAULT NULL,
+  `lastModifiedAt` DATETIME NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS oauth_approvals (
-  userId         VARCHAR(256),
-  clientId       VARCHAR(256),
-  scope          VARCHAR(256),
-  status         VARCHAR(10),
-  expiresAt      TIMESTAMP,
-  lastModifiedAt TIMESTAMP
-);
 
--- customized oauth_client_details table
-CREATE TABLE IF NOT EXISTS ClientDetails (
-  appId                  VARCHAR(256) PRIMARY KEY,
-  resourceIds            VARCHAR(256),
-  appSecret              VARCHAR(256),
-  scope                  VARCHAR(256),
-  grantTypes             VARCHAR(256),
-  redirectUrl            VARCHAR(256),
-  authorities            VARCHAR(256),
-  access_token_validity  INTEGER,
-  refresh_token_validity INTEGER,
-  additionalInformation  VARCHAR(4096),
-  autoApproveScopes      VARCHAR(256)
-);
+-- -----------------------------------------------------
+-- Table `oatuh2`.`oauth_client_details`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`oauth_client_details` (
+  `client_id` VARCHAR(128) NOT NULL,
+  `resource_ids` VARCHAR(256) NULL DEFAULT NULL,
+  `client_secret` VARCHAR(256) NULL DEFAULT NULL,
+  `scope` VARCHAR(256) NULL DEFAULT NULL,
+  `authorized_grant_types` VARCHAR(256) NULL DEFAULT NULL,
+  `web_server_redirect_uri` VARCHAR(256) NULL DEFAULT NULL,
+  `authorities` VARCHAR(256) NULL DEFAULT NULL,
+  `access_token_validity` INT(11) NULL DEFAULT NULL,
+  `refresh_token_validity` INT(11) NULL DEFAULT NULL,
+  `additional_information` VARCHAR(4096) NULL DEFAULT NULL,
+  `autoapprove` VARCHAR(256) NULL DEFAULT NULL,
+  PRIMARY KEY (`client_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `oatuh2`.`oauth_client_token`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`oauth_client_token` (
+  `token_id` VARCHAR(256) NULL DEFAULT NULL,
+  `token` BLOB NULL DEFAULT NULL,
+  `authentication_id` VARCHAR(128) NOT NULL,
+  `user_name` VARCHAR(256) NULL DEFAULT NULL,
+  `client_id` VARCHAR(256) NULL DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `oatuh2`.`oauth_code`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`oauth_code` (
+  `code` VARCHAR(256) NULL DEFAULT NULL,
+  `authentication` BLOB NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `oatuh2`.`oauth_refresh_token`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oauth2db`.`oauth_refresh_token` (
+  `token_id` VARCHAR(256) NULL DEFAULT NULL,
+  `token` BLOB NULL DEFAULT NULL,
+  `authentication` BLOB NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
