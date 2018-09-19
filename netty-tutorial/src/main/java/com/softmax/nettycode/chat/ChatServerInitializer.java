fe425ffdd1e328c7b49d29a5ee08bba919600d6b
Jarvis <qdw8911@sina.com>
2018-09-19 10:33:51 +0800

    netty

---
+++ netty-tutorial/src/main/java/com/softmax/nettycode/chat/ChatServerInitializer.java
@@ -0,0 +1,41 @@
+package com.softmax.nettycode.chat;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * 初始化 ChannelPipeline
+ *
+ * @author Jarvis
+ * @date 2018/9/18
+ */
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+
+    private final ChannelGroup group;
+
+    public ChatServerInitializer(ChannelGroup group) {
+        this.group = group;
+    }
+
+    /**
+     * 设置所有新注册的 Channel 的ChannelPipeline
+     * 安装所有需要的 ChannelHandler
+     */
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpRequestHandler("/ws"));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler());
+    }
+
+}
