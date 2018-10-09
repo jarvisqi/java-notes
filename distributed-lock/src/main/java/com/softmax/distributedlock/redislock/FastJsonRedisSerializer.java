84d410080a099f149a65766f13772861cefd3fe2
Jarvis <qdw8911@sina.com>
2018-10-09 15:47:43 +0800

    redisson 实现分布式锁

--- distributed-lock/src/main/java/com/softmax/distributedlock/redislock/FastJsonRedisSerializer.java
+++ distributed-lock/src/main/java/com/softmax/distributedlock/redislock/FastJsonRedisSerializer.java
@@ -2,7 +2,6 @@ package com.softmax.distributedlock.redislock;
 
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
---
+++ distributed-lock/src/main/java/com/softmax/distributedlock/redissionlock/RedissonProperties.java
@@ -0,0 +1,34 @@
+package com.softmax.distributedlock.redissionlock;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson 配置文件
+ *
+ * @author Jarvis
+ * @date 2018/10/9
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedissonProperties {
+
+    private String address;
+    private String password;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
