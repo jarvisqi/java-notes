c17f4f43a0cf66c0836616bbf7b9a6bb651cfa40
jarvisqi <qdw8911@sina.com>
2018-11-14 16:49:48 +0800

    RocketMQ 分布式事务

---
+++ rockct-tutorial/src/main/java/com/mq/rockct/tutorial/transaction/example/TransactionListenerImpl.java
@@ -0,0 +1,59 @@
+package com.mq.rockct.tutorial.transaction.example;
+
+import org.apache.rocketmq.client.producer.LocalTransactionState;
+import org.apache.rocketmq.client.producer.TransactionListener;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 执行本地事务，检查本地事务
+ * Implement the TransactionListener interface
+ *
+ * @author Jarvis
+ * @date 2018/11/14
+ */
+public class TransactionListenerImpl implements TransactionListener {
+    private AtomicInteger transactionIndex = new AtomicInteger(0);
+    private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
+
+    /**
+     * 执行本地事务
+     *
+     * @param message
+     * @param o
+     * @return
+     */
+    @Override
+    public LocalTransactionState executeLocalTransaction(Message message, Object o) {
+        int value = transactionIndex.getAndIncrement();
+        int status = value % 3;
+        localTrans.put(message.getTransactionId(), status);
+
+        return LocalTransactionState.UNKNOW;
+    }
+
+    /**
+     * 可以根据由MQ回传的key去数据库查询，这条数据到底是成功了还是失败了。
+     *
+     * @param messageExt
+     * @return
+     */
+    @Override
+    public LocalTransactionState checkLocalTransaction(MessageExt messageExt) {
+        Integer status = localTrans.get(messageExt.getTransactionId());
+        if (null != status) {
+            switch (status) {
+                case 0:
+                    return LocalTransactionState.UNKNOW;
+                case 1:
+                    return LocalTransactionState.COMMIT_MESSAGE;
+                default:
+                    return LocalTransactionState.ROLLBACK_MESSAGE;
+            }
+        }
+        return LocalTransactionState.COMMIT_MESSAGE;
+    }
+}
