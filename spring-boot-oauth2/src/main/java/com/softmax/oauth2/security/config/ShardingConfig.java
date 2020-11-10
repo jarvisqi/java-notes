7ef8c52bbfec305c32ad13dc3318e7336c01f03b
jarvisqi <qdw8911@sina.com>
2020-11-10 17:20:36 +0800

    update

--- spring-boot-oauth2/src/main/java/com/softmax/oauth2/security/config/ShardingConfig.java
+++ spring-boot-oauth2/src/main/java/com/softmax/oauth2/security/config/ShardingConfig.java
@@ -1,5 +1,6 @@
 package com.softmax.oauth2.security.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.shardingsphere.api.config.masterslave.LoadBalanceStrategyConfiguration;
@@ -19,13 +20,14 @@ import org.springframework.util.StringUtils;
 
 import javax.naming.ConfigurationException;
 import javax.sql.DataSource;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.*;
 
 /**
  * @author Jarvis
  */
-@SuppressWarnings("unchecked")
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "sharding")
@@ -50,7 +52,6 @@ public class ShardingConfig {
         this.shardingRule = shardingRule;
     }
 
-
     /**
      * 数据节点Key
      */
@@ -66,12 +67,36 @@ public class ShardingConfig {
     private final static String TABLE_STRATEGY = "tableStrategy";
 
     /**
+     * 最小连接池
+     */
+    private final static int MIN_POOL_SIZE = 10;
+
+    /**
+     * 最大连接池
+     */
+    private final static int MAX_POOL_SIZE = 60;
+
+    /**
+     * 初始化最小连接池
+     */
+    private final static int INIT_SIZE = 5;
+
+    /**
+     * 最大等待时间
+     */
+    private final static int MAX_WAIT = 1800000;
+
+    private final static int CONNECTION_TIMEOUT = 30000;
+
+    private final static int IDLE_TIMEOUT = 30000;
+
+    /**
      * 获取数据源对象
      *
      * @throws SQLException
      */
     @Bean
-    public DataSource shardingCreateDataSource() throws SQLException {
+    public DataSource shardingCreateDataSource() throws SQLException, ConfigurationException {
         Map<String, DataSource> dataSourceMap = getDataSourceMap();
         ShardingRuleConfiguration shardingRuleConfig = getShardingRuleConfig();
         DataSource dataSource = ShardingDataSourceFactory.createDataSource(dataSourceMap, shardingRuleConfig, new Properties());
@@ -84,48 +109,91 @@ public class ShardingConfig {
      *
      * @return
      */
-    private Map<String, DataSource> getDataSourceMap() {
+    private Map<String, DataSource> getDataSourceMap() throws ConfigurationException, SQLException {
         Map<String, DataSource> dataSourceMap = new HashMap<>(4);
         if (datasource.isEmpty()) {
-            new ConfigurationException("Data source configuration error");
+            throw new ConfigurationException("Data source configuration error");
         }
 
         Properties properties = new Properties();
-        properties.setProperty("testOnBorrow", "true");
-        properties.setProperty("testWhileIdle", "true");
+        properties.setProperty("testOnBorrow", "false");
         properties.setProperty("testOnReturn", "false");
+        properties.setProperty("testWhileIdle", "true");
         properties.setProperty("validationQuery", "select 'x'");
 
-        for (String ds : datasource.keySet()) {
-            LinkedHashMap<String, String> dbConfigList = (LinkedHashMap<String, String>) datasource.get(ds);
-            String driverClassName = dbConfigList.get("driverClassName");
+        String[] dsNames;
+        try {
+            dsNames = datasource.get("names").toString().split(",");
+        } catch (Exception e) {
+            throw new ConfigurationException("Data source configuration error");
+        }
 
-            String name = dbConfigList.get("alias");
-            String jdbcUrl = dbConfigList.get("url");
-            String dbUsername = dbConfigList.get("username");
-            String dbPassword = dbConfigList.get("password");
+        for (String dsName : dsNames) {
+            LinkedHashMap<String, String> dbConfigMap = (LinkedHashMap<String, String>) datasource.get(dsName);
+            String driverClassName = dbConfigMap.get("driverClassName");
+            String type = dbConfigMap.get("type");
+            String dbUrl = dbConfigMap.get("url");
+            String dbUsername = dbConfigMap.get("username");
+            String dbPassword = dbConfigMap.get("password");
+            Class clsDbType;
+            try {
+                clsDbType = Class.forName(type);
+            } catch (ClassNotFoundException e) {
+                throw new ConfigurationException("Data source type configuration error");
+            }
+            Object obj;
+            try {
+                obj = clsDbType.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                throw new ConfigurationException("Data source type configuration error");
+            }
 
-            HikariConfig hikariConfig = new HikariConfig();
-            hikariConfig.setDriverClassName(driverClassName);
-            hikariConfig.setJdbcUrl(jdbcUrl);
-            hikariConfig.setUsername(dbUsername);
-            hikariConfig.setPassword(dbPassword);
+            if (obj instanceof HikariDataSource) {
+                HikariConfig hikariConfig = new HikariConfig();
+                hikariConfig.setDriverClassName(driverClassName);
+                hikariConfig.setJdbcUrl(dbUrl);
+                hikariConfig.setUsername(dbUsername);
+                hikariConfig.setPassword(dbPassword);
 
-            //等待连接池分配连接的最大时长（毫秒），超过这个时长还没可用的连接则发生SQLException， 缺省:30秒
-            hikariConfig.setConnectionTimeout(30000);
-            //一个连接idle状态的最大时长（毫秒），超时则被释放（retired），缺省:10分钟
-            hikariConfig.setIdleTimeout(30000);
-            //一个连接的生命时长（毫秒），超时而且没被使用则被释放（retired），缺省:30分钟，建议设置比数据库超时时长少30秒，
-            // 参考MySQL wait_timeout参数（show variables like '%timeout%';）
-            hikariConfig.setMaxLifetime(1800000);
-            //连接池中允许的最大连接数。缺省值：10；推荐的公式：((core_count * 2) + effective_spindle_count)
-            hikariConfig.setMaximumPoolSize(60);
-            hikariConfig.setMinimumIdle(10);
-            hikariConfig.setDataSourceProperties(properties);
+                //等待连接池分配连接的最大时长（毫秒），超过这个时长还没可用的连接则发生SQLException， 缺省:30秒
+                hikariConfig.setConnectionTimeout(CONNECTION_TIMEOUT);
+                //一个连接idle状态的最大时长（毫秒），超时则被释放（retired），缺省:10分钟
+                hikariConfig.setIdleTimeout(IDLE_TIMEOUT);
+                //一个连接的生命时长（毫秒），超时而且没被使用则被释放（retired），缺省:30分钟，建议设置比数据库超时时长少30秒，
+                // 参考MySQL wait_timeout参数（show variables like '%timeout%';）
+                hikariConfig.setMaxLifetime(MAX_WAIT);
+                //连接池中允许的最大连接数。缺省值：10；推荐的公式：((core_count * 2) + effective_spindle_count)
+                hikariConfig.setMaximumPoolSize(MAX_POOL_SIZE);
+                hikariConfig.setMinimumIdle(MIN_POOL_SIZE);
+                hikariConfig.setDataSourceProperties(properties);
 
-            HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-            logger.info("-------------------Init HikariDataSource {} ---------------------------", name);
-            dataSourceMap.put(ds, dataSource);
+                HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+                logger.info("-------------------Init HikariDataSource {} ---------------------------", dsName);
+                dataSourceMap.put(dsName, dataSource);
+            } else if (obj instanceof DruidDataSource) {
+                DruidDataSource druidDataSource = new DruidDataSource();
+                druidDataSource.setUsername(dbUsername);
+                druidDataSource.setPassword(dbPassword);
+                druidDataSource.setUrl(dbUrl);
+                druidDataSource.setDriverClassName(driverClassName);
+
+                druidDataSource.setFilters("stat");
+                druidDataSource.setInitialSize(INIT_SIZE);
+                druidDataSource.setMinIdle(MIN_POOL_SIZE);
+                druidDataSource.setMaxActive(MAX_POOL_SIZE);
+                // 连接等待超时的时间
+                druidDataSource.setMaxWait(MAX_WAIT);
+                // 配置间隔多久才进行一次检测，检测需要关闭的空闲连接，单位是毫秒
+                druidDataSource.setTimeBetweenEvictionRunsMillis(2000);
+                //连接在池中最大生存的时间，单位是毫秒
+                druidDataSource.setMaxEvictableIdleTimeMillis(900000);
+                //连接在池中最小生存的时间，单位是毫秒
+                druidDataSource.setMinEvictableIdleTimeMillis(600000);
+
+                druidDataSource.setUseGlobalDataSourceStat(true);
+            } else {
+                throw new ConfigurationException("Data source  configuration error");
+            }
         }
         return dataSourceMap;
     }
@@ -135,10 +203,10 @@ public class ShardingConfig {
      *
      * @return ShardingRuleConfiguration
      */
-    private ShardingRuleConfiguration getShardingRuleConfig() {
+    private ShardingRuleConfiguration getShardingRuleConfig() throws ConfigurationException {
 
         if (shardingRule.isEmpty()) {
-            new ConfigurationException("Sharding rule configuration error");
+            throw new ConfigurationException("Sharding rule configuration error");
         }
         // 配置规则
         ShardingRuleConfiguration shardingRuleConfiguration = new ShardingRuleConfiguration();
@@ -156,10 +224,10 @@ public class ShardingConfig {
      *
      * @return
      */
-    private List<TableRuleConfiguration> getTableRuleConfigurations() {
+    private List<TableRuleConfiguration> getTableRuleConfigurations() throws ConfigurationException {
         LinkedHashMap<String, Object> ruleConfigList = (LinkedHashMap<String, Object>) shardingRule.get("tables");
         if (ruleConfigList.isEmpty()) {
-            new ConfigurationException("Table rule configuration error");
+            throw new ConfigurationException("Table rule configuration error");
         }
         Set<String> tbNames = ruleConfigList.keySet();
         List<TableRuleConfiguration> tableRuleConfigurations = new ArrayList<>(8);
@@ -169,7 +237,7 @@ public class ShardingConfig {
             //数据节点
             String actualDataNodes = strategyConfigMap.get(ACTUAL_DATANODES).toString();
             if (!StringUtils.isEmpty(actualDataNodes)) {
-                new ConfigurationException(ACTUAL_DATANODES + " configuration error");
+                throw new ConfigurationException(ACTUAL_DATANODES + " configuration error");
             }
 
             for (String configKey : strategyConfigMap.keySet()) {
@@ -208,14 +276,14 @@ public class ShardingConfig {
     /**
      * 读写分离规则
      */
-    private List<MasterSlaveRuleConfiguration> getMasterSlaveRules() {
+    private List<MasterSlaveRuleConfiguration> getMasterSlaveRules() throws ConfigurationException {
         LinkedHashMap<String, Object> ruleConfigList = (LinkedHashMap<String, Object>) shardingRule.get("masterSlaveRules");
         if (ruleConfigList.isEmpty()) {
-            new ConfigurationException("The read-write separation rule is configured incorrectly or not configured");
+            throw new ConfigurationException("The read-write separation rule is configured incorrectly or not configured");
         }
         Set<String> instanceNames = ruleConfigList.keySet();
         if (instanceNames.isEmpty()) {
-            new ConfigurationException("Misconfiguration of read-write separation rules");
+            throw new ConfigurationException("Misconfiguration of read-write separation rules");
         }
         List<MasterSlaveRuleConfiguration> masterSlaveRuleConfigurations = new ArrayList<>(4);
         for (String name : instanceNames) {
