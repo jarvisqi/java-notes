fc99c19055fba149e80de13483b380fc8e90f310
jarvisqi <qdw8911@sina.com>
2022-03-31 11:47:50 +0800

    打印三角形测试

--- basic-tutorial/src/main/java/com/softmax/leet/PascalTriangle.java
+++ basic-tutorial/src/main/java/com/softmax/leet/PascalTriangle.java
@@ -69,15 +69,48 @@ public class PascalTriangle {
 
     }
 
+    /**
+     * 打印三角形数字
+     */
+    public static void printForwardTriangleOfNum(int n) {
+        //创建二维数组
+        int[][] triangle = new int[n][];
+        //遍历二维数组的第一层
+        for (int i = 0; i < triangle.length; i++) {
+            //初始化第二层数组的大小
+            triangle[i] = new int[i];
+            //遍历第二层数组
+            for (int j = 0; j < triangle[i].length; j++) {
+                //两侧的数组元素赋值为1
+                if (i == 0 || j == 0 || j == triangle[i].length - 1) {
+                    //两侧的数组元素赋值为1
+                    triangle[i][j] = 1;
+                } else {
+                    //其他数值通过公式计算
+                    triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+                }
+                //输出数组元素
+                System.out.print(triangle[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    /**
+     * 打印三角形数字
+     */
+    public static void pringInvertedTriangleOfNum() {
+
+    }
+
     public static void main(String[] args) {
         printForwardTriangle(5);
-
         System.out.println("\n ================================================");
-
         pringInvertedTriangle(5);
-
         System.out.println("\n ================================================");
-
         printMultiplicationTable();
+        System.out.println("\n ================================================");
+        printForwardTriangleOfNum(8);
     }
 }
