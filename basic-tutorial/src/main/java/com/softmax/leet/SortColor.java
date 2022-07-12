d07505401f18bf30f9187dc52103198cabf80b1a
jarvisqi <qdw8911@foxmail.com>
2022-07-12 15:10:59 +0800

    update

--- basic-tutorial/src/main/java/com/softmax/leet/SortColor.java
+++ basic-tutorial/src/main/java/com/softmax/leet/SortColor.java
@@ -2,6 +2,8 @@ package com.softmax.leet;
 
 /**
  * 荷兰国旗问题
+ * 荷兰国旗是由红白蓝3种颜色的条纹拼接而成,假设这样的条纹有多条，且各种颜色的数量不一，并且随机组成了一个新的图形
+ * 把这些条纹按照颜色排好，红色的在上半部分，白色的在中间部分，蓝色的在下半部分，我们把这类问题称作荷兰国旗问题。
  */
 public class SortColor {
     private static void sort(int[] arr) {
