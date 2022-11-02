a47817385b4e7e86ac99b41831218973437a506f
jarvisqi <qdw8911@foxmail.com>
2022-11-02 15:59:40 +0800

    Kriging update

---
+++ gradle-spring-learn/src/main/java/org/softmax/gradle/kriging/bean/Tuple2.java
@@ -0,0 +1,20 @@
+package org.softmax.gradle.kriging.bean;
+
+public class Tuple2<A, B> {
+
+	public final A _1;
+	public final B _2;
+
+	public Tuple2(A a, B b) {
+		this._1 = a;
+		this._2 = b;
+	}
+
+	@Override
+	public String toString() {
+		return "(" +
+				_1 +
+				","+ _2 +
+				')';
+	}
+}
---
+++ gradle-spring-learn/src/main/java/org/softmax/gradle/kriging/bean/Tuple3.java
@@ -0,0 +1,23 @@
+package org.softmax.gradle.kriging.bean;
+
+public class Tuple3<A, B, C> {
+
+	public final A _1;
+	public final B _2;
+	public final C _3;
+
+	public Tuple3(A a, B b, C c) {
+		this._1 = a;
+		this._2 = b;
+		this._3 = c;
+	}
+
+	@Override
+	public String toString() {
+		return "(" +
+				_1 +
+				","+ _2 +
+				","+ _3 +
+				')';
+	}
+}
