d589ada8d0fab3adaa9a777c78ab9722c1401ddc
jarvisqi <qdw8911@foxmail.com>
2022-11-03 14:45:20 +0800

    border update

---
+++ gradle-spring-learn/src/main/java/org/softmax/gradle/kriging/draw/spatial/IJPoint.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2012 Yaqiang Wang,
+ * yaqiang.wang@gmail.com
+ */
+package org.softmax.gradle.kriging.draw.spatial;
+
+/**
+ * Point integer, to indicate the position in grid data
+ *
+ * @author Yaqiang Wang
+ */
+public class IJPoint {
+
+    public int I; // row in matrix
+    public int J; // column in matrix
+}
