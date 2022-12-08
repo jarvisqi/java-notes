8c8997f6cb9b08261b3ef863e8876cb2363b58bf
jarvisqi <qdw8911@foxmail.com>
2022-12-08 13:40:15 +0800

    bigdecimal utils

---
+++ basic-tutorial/src/main/java/com/softmax/basic/datastructure/sample/DecimalUtils.java
@@ -0,0 +1,58 @@
+package com.softmax.basic.datastructure.sample;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * @author Jarvis
+ */
+public class DecimalUtils {
+    /**
+     * 默认除法运算精度
+     */
+    private static final int DEF_DIV_SCALE = 10;
+
+    /**
+     * 提供精确的加法运算
+     *
+     * @param v1 被加数
+     * @param v2 加数
+     * @return 两个参数的和
+     */
+    public static double add(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的加法运算
+     *
+     * @param v1 被加数
+     * @param v2 加数
+     * @return 两个参数的和
+     */
+    public static BigDecimal add(String v1, String v2) {
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.add(b2);
+    }
+
+    /**
+     * 提供精确的加法运算
+     *
+     * @param v1    被加数
+     * @param v2    加数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的和
+     */
+    public static String add(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                    "The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.add(b2).setScale(scale, RoundingMode.HALF_UP).toString();
+    }
+}
