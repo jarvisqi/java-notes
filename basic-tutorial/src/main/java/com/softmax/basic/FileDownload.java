fb55d9ea0ec2175e661612966186411cce6f15ee
jarvisqi <qdw8911@foxmail.com>
2022-12-28 10:35:14 +0800

    bigdecimal subtract

--- basic-tutorial/src/main/java/com/softmax/basic/FileDownload.java
+++ basic-tutorial/src/main/java/com/softmax/basic/FileDownload.java
@@ -9,7 +9,8 @@ public class FileDownload {
 
 
     public static void main(String[] args) throws IOException {
-        download();
+//        download();
+        url();
     }
 
     public static void download() throws IOException {
@@ -17,6 +18,17 @@ public class FileDownload {
         String fullUrl = "https://pdf.dfcfw.com/pdf/H3_AP202205071564055035_1.pdf?1651920741000.pdf";
         HttpUtil.downloadFile(fullUrl, FileUtil.file(fineName));
         System.out.println("finished");
+
+    }
+
+    public static void url(){
+        //匹配ip的正则
+        String reg = "http?://(\\d+\\.\\d+\\.\\d+\\.\\d+):(\\d+)";
+        String str = "http://127.0.0.1:10616/cus/assemble/openapi/v1/signlepoint/getsingleAuthUrl?v=%7B%22clientId%22%3A%22econtract%22%2C%22userAccount%22%3A%2202068755f0624a82%22%2C%22page%22%3A%22singleAuth%22%7D";
+        String baidu = "https://ca-minlist.minmetals.com.cn/ecapi";
+        String endStr = str.replaceAll(reg, baidu);
+        System.out.println("替换前：" + str);
+        System.out.println("替换后：" + endStr);
 
     }
 }
