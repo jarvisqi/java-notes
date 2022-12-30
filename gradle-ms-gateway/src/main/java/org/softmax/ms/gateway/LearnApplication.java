aaa9409d701b64c4379d600de6dacba1e458ab28
jarvisqi <qdw8911@foxmail.com>
2022-12-30 14:54:14 +0800

    EnableDiscoveryClient

--- gradle-ms-gateway/src/main/java/org/softmax/ms/gateway/LearnApplication.java
+++ gradle-ms-gateway/src/main/java/org/softmax/ms/gateway/LearnApplication.java
@@ -1,4 +1,4 @@
-package org.softmax.gradle;
+package org.softmax.ms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
