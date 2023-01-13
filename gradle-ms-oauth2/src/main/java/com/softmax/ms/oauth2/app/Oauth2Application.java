aa5da411c3d558856158af752534dc2056bc7ce0
jarvisqi <qdw8911@foxmail.com>
2023-01-13 16:49:41 +0800

    update nacos address

--- gradle-ms-oauth2/src/main/java/com/softmax/ms/oauth2/app/Oauth2Application.java
+++ gradle-ms-oauth2/src/main/java/com/softmax/ms/oauth2/app/Oauth2Application.java
@@ -1,4 +1,4 @@
-package com.softmax.ms.oauth2;
+package com.softmax.ms.oauth2.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
