5fe4667c59b14e9cf448fa73a3f6b74c2739fb82
jarvisqi <qdw8911@foxmail.com>
2023-02-24 16:41:31 +0800

    update

---
+++ basic-tutorial/src/main/java/com/softmax/basic/ellipse/XuzsMathMin.java
@@ -0,0 +1,24 @@
+package com.softmax.basic.ellipse;
+
+public class XuzsMathMin {
+
+    private float num;
+
+    private int iMin;
+
+    public float getNum() {
+        return num;
+    }
+
+    public void setNum(float num) {
+        this.num = num;
+    }
+
+    public int getiMin() {
+        return iMin;
+    }
+
+    public void setiMin(int iMin) {
+        this.iMin = iMin;
+    }
+}
