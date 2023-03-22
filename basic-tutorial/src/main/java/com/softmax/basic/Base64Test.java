6eb35b579ae8c1d0040913a806191ff0f4a820f8
jarvisqi <qdw8911@foxmail.com>
2023-03-22 14:52:22 +0800

    测试String性能

--- basic-tutorial/src/main/java/com/softmax/basic/Base64Test.java
+++ basic-tutorial/src/main/java/com/softmax/basic/Base64Test.java
@@ -1,8 +1,15 @@
 package com.softmax.basic;
 
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson2.JSON;
+
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 /**
  * @author : Jarvis
@@ -11,7 +18,33 @@ import java.util.Base64;
 public class Base64Test {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        base64Test();
+//        base64Test();
+
+        JsonInfo info = new JsonInfo(1, 2, "title", "name", "desc");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add("AAAAAAAA");
+        }
+        info.setObjs(list);
+
+        String jsonStr = JSONUtil.toJsonStr(info);
+        System.out.println("len:" + jsonStr.length());
+
+        Instant now = Instant.now();
+        for (int i = 0; i < 10000; i++) {
+            testJson(jsonStr);
+        }
+        Instant end = Instant.now();
+        long timeElapsed = Duration.between(now, end).toMillis();
+        System.out.println(timeElapsed);
+
+        Instant now1 = Instant.now();
+        for (int i = 0; i < 10000; i++) {
+            testFastJson(jsonStr);
+        }
+        Instant end1 = Instant.now();
+        long timeElapsed1 = Duration.between(now1, end1).toMillis();
+        System.out.println(timeElapsed1);
     }
 
     private static void base64Test() throws UnsupportedEncodingException {
@@ -28,5 +61,79 @@ public class Base64Test {
         System.out.println("URL解码：" + new String(base64UrlBytes, StandardCharsets.UTF_8));
     }
 
+    private static void testJson(String jsonStr) {
+        JSONUtil.toBean(jsonStr, JsonInfo.class);
+    }
+
+    private static void testFastJson(String jsonStr) {
+        JSON.parseObject(jsonStr, JsonInfo.class);
+    }
+
+
+    static class JsonInfo {
+        private Integer price;
+        private Integer rate;
+        private String title;
+        private String name;
+        private String desc;
+
+        public List<String> getObjs() {
+            return objs;
+        }
+
+        public void setObjs(List<String> objs) {
+            this.objs = objs;
+        }
+
+        private List<String> objs;
+
+        public JsonInfo(Integer price, Integer rate, String title, String name, String desc) {
+            this.price = price;
+            this.rate = rate;
+            this.title = title;
+            this.name = name;
+            this.desc = desc;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+
+        public Integer getRate() {
+            return rate;
+        }
+
+        public void setRate(Integer rate) {
+            this.rate = rate;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
 
 }
