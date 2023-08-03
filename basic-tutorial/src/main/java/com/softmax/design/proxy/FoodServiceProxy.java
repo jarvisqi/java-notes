46d8ad5efe91bba3da8c553cbdfc92b49c0fb739
jarvisqi <qdw8911@foxmail.com>
2023-08-03 15:50:37 +0800

    update proxy pattern

--- basic-tutorial/src/main/java/com/softmax/design/proxy/FoodServiceProxy.java
+++ basic-tutorial/src/main/java/com/softmax/design/proxy/FoodServiceProxy.java
@@ -102,21 +102,5 @@ class FoodServiceImpl implements FoodService {
     }
 }
 
-/**
- * 客户端调用，注意，我们要用代理来实例化接口：
- */
-class ProxyApp {
-    public static void main(String[] args) {
-        //创建代理对象
-        FoodService serviceProxy = new FoodServiceProxy(new FoodServiceImpl());
-        //使用代理对象 制作鸡肉饭
-        var chicken = serviceProxy.createChicken();
-        System.out.println(chicken.toString());
-        //使用代理对象 制作牛肉面
-        var noodle = serviceProxy.createNoodle();
-        System.out.println(noodle.toString());
-    }
-}
-
 
 
