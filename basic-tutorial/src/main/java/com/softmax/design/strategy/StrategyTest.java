1a07360d48958012bf43aef7a9a616e78bf8a852
jarvisqi <qdw8911@foxmail.com>
2023-08-04 16:54:47 +0800

    update strategy pattern

--- basic-tutorial/src/main/java/com/softmax/design/strategy/StrategyTest.java
+++ basic-tutorial/src/main/java/com/softmax/design/strategy/StrategyTest.java
@@ -4,25 +4,22 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * 客户端调用
- * 策略模式
- * 有新的算法是不需要再更改任何现有代码的，只需要新写一个算法比如乘法Multiplication，
- * 并实现calculate方法，接下来要做的只是组装上去便可以使用了。
+ * 绛栫暐妯″紡
  *
  * @author Jarvis
  * @date 2019/10/22
  */
 
-public class Client {
+public class StrategyTest {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        //加法计算器
+        //鍔犳硶
         calculator.setCalculateStrategy(new Addition());
         int result = calculator.getResult(1, 2);
         System.out.println(result);
 
-        //减法
+        //鍑忔硶
         calculator.setCalculateStrategy(new Substanction());
         result = calculator.getResult(1, 2);
         System.out.println(result);
