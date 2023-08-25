f61d719ce3d75558af82d157079db04d0d0c80ec
jarvisqi <qdw8911@foxmail.com>
2023-08-25 09:13:53 +0800

    influxDB

--- gradle-ms-meteo/src/main/java/org/softmax/ms/meteo/MeteoApplication.java
+++ gradle-ms-meteo/src/main/java/org/softmax/ms/meteo/MeteoApplication.java
@@ -3,7 +3,7 @@ package org.softmax.ms.meteo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.softmax.ms.meteo.*")
 public class MeteoApplication {
 
     public static void main(String[] args) {
