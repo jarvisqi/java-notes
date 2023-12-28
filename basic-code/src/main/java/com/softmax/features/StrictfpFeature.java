840513bbd9319ba0474d9eb92a9b4c221d57e5ad
jarvisqi <qdw8911@foxmail.com>
2023-12-28 09:39:15 +0800

    strictfp feature

--- basic-code/src/main/java/com/softmax/features/StrictfpFeature.java
+++ basic-code/src/main/java/com/softmax/features/StrictfpFeature.java
@@ -4,6 +4,9 @@ public class StrictfpFeature {
 
     public static void main(String[] args) {
         testStrictfp();
+
+        oldStrictfp();
+
     }
 
     public strictfp static void testStrictfp() {
@@ -12,4 +15,12 @@ public class StrictfpFeature {
         double sum = aFloat + aDouble;
         System.out.println("sum: " + sum);
     }
+
+    public  static void oldStrictfp() {
+        float aFloat = 0.6666666666666666666f;
+        double aDouble = 0.88888888888888888d;
+        double sum = aFloat + aDouble;
+        System.out.println("sum: " + sum);
+    }
+
 }
