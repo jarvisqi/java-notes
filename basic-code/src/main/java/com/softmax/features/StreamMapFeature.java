72d1b15bd5b7033b69c7b537c83927cd78799e67
jarvisqi <qdw8911@foxmail.com>
2024-01-16 14:06:34 +0800

    map feature

--- basic-code/src/main/java/com/softmax/features/StreamMapFeature.java
+++ basic-code/src/main/java/com/softmax/features/StreamMapFeature.java
@@ -3,7 +3,11 @@ package com.softmax.features;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StreamMapFeature {
@@ -78,8 +82,16 @@ public class StreamMapFeature {
         System.out.println("---------------------------");
         studentNames.stream().map(s -> s.getMathScore()).forEach(System.out::println);
 
+        Map<String, Integer> map = students.stream().filter(distinctByKey(Student::getName))
+                .collect(Collectors.toMap(Student::getName, Student::getMathScore));
+        System.out.println(map);
+
     }
 
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
 
     class Student implements Serializable {
         /**
