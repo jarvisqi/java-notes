58b62bf689e898aeb3b02abe72538502f5ffc09d
jarvisqi <qdw8911@foxmail.com>
2024-01-18 14:07:07 +0800

    update message queue

--- message-queue/src/main/java/com/softmax/mq/courier/rocket/example/OnewayProducer.java
+++ message-queue/src/main/java/com/softmax/mq/courier/rocket/example/OnewayProducer.java
@@ -1,14 +1,10 @@
 package com.softmax.mq.courier.rocket.example;
 
-import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.*;
+
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
-import java.io.UnsupportedEncodingException;
 
 /**
  * 单向传输用于需要中等可靠性的情况，只管发送，不需要返回结果，如日志收集。
@@ -40,32 +36,3 @@ public class OnewayProducer {
     }
 }
 
-class OnewayConsumer {
-
-    public static void main(String[] args) throws Exception {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ONEWAY_CONSUMER");
-        consumer.setNamesrvAddr(SyncProducer.NAMESRVADDR);
-        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        //设置consumer所订阅的Topic和Tag，*代表全部的Tag
-        consumer.subscribe("Oneway_Topic_Test", "*");
-
-        consumer.registerMessageListener((MessageListenerConcurrently) (list, consumeOrderlyContext) -> {
-            MessageExt messge = list.get(0);
-            try {
-                String topic = messge.getTopic();
-                String tags = messge.getTags();
-                String msgBody = new String(messge.getBody(), "utf-8");
-                System.out.println("收到消息:topic:" + topic + ",tags:" + tags + ",msg:" + messge);
-                System.out.println("MsgBody:" + msgBody);
-                System.out.println("------------------------------------------------------------------------------");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-        });
-        consumer.start();
-        System.out.println("consumer start");
-
-    }
-}
