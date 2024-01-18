562388761599e814d369552c0c3f130a41edb9b0
jarvisqi <qdw8911@foxmail.com>
2024-01-18 15:06:58 +0800

    update message queue

--- message-queue/src/main/java/com/softmax/mq/courier/rocket/example/DelayProduceTest.java
+++ message-queue/src/main/java/com/softmax/mq/courier/rocket/example/DelayProduceTest.java
@@ -1,17 +1,18 @@
 package com.softmax.mq.courier.rocket.example;
 
-public class DelayProduceTest {
+import org.springframework.boot.CommandLineRunner;
 
+public class DelayProduceTest implements CommandLineRunner {
 
     private static DelayProduce delayProduce = new DelayProduce();
 
     /**
-     * RocketMQ不支持任意时间的延时，只支持以下几个固定的延时等级
+     * 注意：RocketMQ不支持任意时间的延时，只支持以下几个固定的延时等级
      */
     private String messageDelayLevel = "1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h";
 
-    public static void main(String[] args) {
+    @Override
+    public void run(String... args) throws Exception {
         delayProduce.sendDelayMessage("delay-topic", "Hello RocketMQ", 5);
     }
-
 }
