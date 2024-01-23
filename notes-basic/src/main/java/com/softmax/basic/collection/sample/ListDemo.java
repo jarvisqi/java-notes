efc558e8faa99c17e50f180b7ac7b0f46db1a6a1
jarvisqi <qdw8911@foxmail.com>
2024-01-23 09:26:09 +0800

    collection list

---
+++ notes-basic/src/main/java/com/softmax/basic/collection/sample/ListDemo.java
@@ -0,0 +1,51 @@
+package com.softmax.basic.collection.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Jarvis
+ * @date 2018/9/13
+ */
+public class ListDemo {
+
+    public static void main(String[] args) {
+        List<Integer> listA = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            listA.add(i);
+        }
+
+        List<Integer> listB = new ArrayList<>();
+        listB.add(2);
+        listB.add(3);
+        listB.add(7);
+        listB.add(8);
+
+        System.out.println(listA);
+        System.out.println(listB);
+
+
+        //交集
+        List<Integer> intersection = listA.stream().filter(a -> listB.contains(a)).collect(Collectors.toList());
+        System.out.println("----交集");
+        System.out.println(intersection);
+
+        //差集
+        List<Integer> reduce1 = listA.stream().filter(a -> !listB.contains(a)).collect(Collectors.toList());
+        System.out.println("----交集 listA - listB");
+        System.out.println(reduce1);
+
+        //并集
+        listA.addAll(listB);
+        System.out.println("---并集 listAll---");
+        System.out.println(listA);
+
+        //并集去重
+        List<Integer> listAllDistinct = listA.stream().distinct().collect(Collectors.toList());
+
+        System.out.println("---并集去重---");
+        System.out.println(listAllDistinct);
+    }
+
+}
