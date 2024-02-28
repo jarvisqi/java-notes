4d13d335ed7648b46c266c173a57ec40f2c41d8a
jarvisqi <qdw8911@foxmail.com>
2024-02-28 09:47:56 +0800

    update test

---
+++ notes-basic/src/test/java/TestTransactional.java
@@ -0,0 +1,31 @@
+import com.softmax.spring.service.AccountService;
+import com.softmax.spring.service.impl.AccountServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class TestTransactional {
+
+    @Parameterized.Parameter(0)
+    public AccountService accountService;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> params = new ArrayList<>();
+        params.add(new Object[]{new AccountServiceImpl()});
+        return params;
+    }
+
+
+    @Test
+    public void testSave() {
+
+        accountService.saveSomething();
+
+    }
+
+}
