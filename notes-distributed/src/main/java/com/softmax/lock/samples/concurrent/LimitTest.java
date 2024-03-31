e401425a77382df347f8c72f38cb5bc4ce4d6964
jarvisqi <qdw8911@foxmail.com>
2024-03-31 21:29:59 +0800

    limit test

---
+++ notes-distributed/src/main/java/com/softmax/lock/samples/concurrent/LimitTest.java
@@ -0,0 +1,40 @@
+package com.softmax.lock.samples.concurrent;
+
+import java.util.Random;
+
+/**
+ * @author Jarvis
+ */
+public class LimitTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        SlidingTimeWindow timeWindow = new SlidingTimeWindow();
+        //开启一个线程判断当前的限流情况.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    timeWindow.doCheck();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        while (true) {
+            //判断是否被限流了.
+            if (!timeWindow.isLimit) {
+                timeWindow.counter++;
+                //未被限流执行相应的业务方法.
+                //  executeBusinessCode();
+                //模拟业务执行方法时间.
+                Thread.sleep(new Random().nextInt(15));
+                System.out.println("业务方法执行完了...");
+            } else {
+                System.out.println("被限流了，直接返回给用户");
+            }
+        }
+    }
+
+
+}
