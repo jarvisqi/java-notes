1e4e5aec1fc534c4cd9c1e5debaea8f60c4672ee
jarvisqi <qdw8911@foxmail.com>
2024-04-01 21:41:14 +0800

    redission

---
+++ notes-distributed/src/test/java/com/softmax/lock/samples/DistributedlockApplicationTests.java
@@ -0,0 +1,83 @@
+package com.softmax.lock.samples;
+
+import com.softmax.lock.samples.redislock.UsrInfo;
+import com.softmax.lock.samples.redislock.MsService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DistributedlockApplicationTests {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private MsService service;
+
+    @Test
+    public void set() {
+        redisTemplate.opsForValue().set("MSKey", 10013);
+    }
+
+
+    @Test
+    public void get() {
+        Object val = redisTemplate.opsForValue().get("MSKey");
+        if (val != null) {
+            System.out.println(val.toString());
+        }
+        Integer rev = Integer.valueOf(val.toString());
+        System.out.println("Redis GET: " + rev);
+    }
+
+    @Test
+    public void delete() {
+        boolean val = redisTemplate.delete("MSKey");
+        System.out.println("Redis deleted: " + val);
+    }
+
+
+    @Test
+    public void hash() throws InterruptedException {
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        UsrInfo info1 = new UsrInfo(1001, "Hong");
+        UsrInfo info2 = new UsrInfo(1002, "Kong");
+        //is exist
+        if (hashOperations.getOperations().hasKey("info_1001")) {
+            //delete
+            hashOperations.delete("info_1001", "1001");
+            hashOperations.delete("info_1002", "1002");
+            Thread.sleep(3000);
+        }
+        //put
+        hashOperations.put("info_1001", "1001", info1);
+        hashOperations.put("info_1002", "1002", info2);
+        //get
+        UsrInfo info = (UsrInfo) hashOperations.get("info_1001", "1001");
+
+        System.out.println();
+        System.out.println(info);
+
+
+    }
+
+    @Test
+    public void seckill() {
+        System.out.println("开始");
+        //创建线程池
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.execute(() -> service.seckill(redisTemplate, "MSKEY"));
+    }
+
+
+}
+
