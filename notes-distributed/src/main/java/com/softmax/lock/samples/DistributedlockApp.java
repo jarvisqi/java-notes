50034e5b75d9721e7c058fc136de3811f89bc3bb
jarvisqi <qdw8911@foxmail.com>
2024-08-20 09:22:19 +0800

    update

---
+++ notes-distributed/src/main/java/com/softmax/lock/samples/DistributedlockApp.java
@@ -0,0 +1,18 @@
+package com.softmax.lock.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 分布式锁
+ *
+ * @author Jarvis
+ * @date 2018/08/01
+ */
+@SpringBootApplication
+public class DistributedlockApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedlockApp.class, args);
+    }
+}
