b9d7d8d6e10dbf4b42b7c621b954f62d26e0d29f
jarvisqi <qdw8911@foxmail.com>
2024-11-27 15:48:24 +0800

    add entity

---
+++ notes-spring-boot/src/main/java/com/softmax/marvel/entity/TOrder.java
@@ -0,0 +1,17 @@
+package com.softmax.marvel.entity;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * t_order0
+ * @author 
+ */
+@Data
+public class TOrder implements Serializable {
+    private Integer sysno;
+
+    private Long orderId;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ notes-spring-boot/src/main/java/com/softmax/marvel/entity/TUser.java
@@ -0,0 +1,17 @@
+package com.softmax.marvel.entity;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * t_user0
+ * @author 
+ */
+@Data
+public class TUser implements Serializable {
+    private Integer sysno;
+
+    private String name;
+
+    private static final long serialVersionUID = 1L;
+}
