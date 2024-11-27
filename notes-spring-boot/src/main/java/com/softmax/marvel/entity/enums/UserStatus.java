153c5e9105daf7a3edfcdd8e41bdd357daf49fbe
jarvisqi <qdw8911@foxmail.com>
2024-11-27 15:47:56 +0800

    add enums

---
+++ notes-spring-boot/src/main/java/com/softmax/marvel/entity/enums/UserStatus.java
@@ -0,0 +1,26 @@
+package com.softmax.marvel.entity.enums;
+
+import com.framework.common.BaseEnum;
+
+/**
+ * 用户状态
+ *
+ * @author : Jarvis
+ * @date : 2018/5/12
+ */
+public enum UserStatus implements BaseEnum {
+
+    NORMAL(0, "正常"),
+
+    DISABLED(1, "禁用"),
+
+    BANNEDPOST(2, "禁言");
+
+    Integer value;
+    String description;
+
+    UserStatus(Integer value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+}
