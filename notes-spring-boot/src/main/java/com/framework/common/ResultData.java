ad7793ef162e843094c810fb16c0a2d890e90cfc
jarvisqi <qdw8911@foxmail.com>
2024-12-13 11:29:18 +0800

    add response data

---
+++ notes-spring-boot/src/main/java/com/framework/common/ResultData.java
@@ -0,0 +1,98 @@
+package com.framework.common;
+
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * 分页数据类
+ *
+ * @author : Jarvis
+ * @date : 2018/6/1
+ */
+public class ResultData<T> {
+    /**
+     * 当前页
+     */
+    private Integer pageNum = 1;
+    /**
+     * 每页显示的总条数
+     */
+    private Integer pageSize = 10;
+    /**
+     * 总条数
+     */
+    private Integer totalNum;
+    /**
+     * 总页数
+     */
+    private Integer totalPage;
+    /**
+     * 是否有下一页
+     */
+    private Boolean isMore;
+    /**
+     * 分页结果
+     */
+    private List<T> items;
+
+    public ResultData() {
+        super();
+    }
+
+    public ResultData(PageInfo info) {
+        this.totalNum = (int) info.getTotal();
+        this.pageNum = info.getPageNum();
+        this.totalPage = info.getPages();
+        this.items = info.getList();
+        this.isMore = info.getPageNum() < info.getPages();
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getTotalNum() {
+        return totalNum;
+    }
+
+    public void setTotalNum(Integer totalNum) {
+        this.totalNum = totalNum;
+    }
+
+    public Boolean getIsMore() {
+        return isMore;
+    }
+
+    public void setIsMore(Boolean isMore) {
+        this.isMore = isMore;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
+    }
+}
