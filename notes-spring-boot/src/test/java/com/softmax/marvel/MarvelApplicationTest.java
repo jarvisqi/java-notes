f7b62884cb3a30c0b1cbcd382dec5e73f3822d3b
jarvisqi <qdw8911@foxmail.com>
2024-12-13 10:41:43 +0800

    add test

---
+++ notes-spring-boot/src/test/java/com/softmax/marvel/MarvelApplicationTest.java
@@ -0,0 +1,27 @@
+package com.softmax.marvel;
+
+import com.softmax.marvel.MarvelApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Jarvis
+ * @date 2018/10/17
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MarvelApplication.class)
+public class MarvelApplicationTest {
+
+    @Test
+    public void memoryTest() {
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        System.out.println(maxMemory / 1024 / 1024 / 1024);
+        System.out.println(freeMemory / 1024 / 1024 / 1024);
+        System.out.println(totalMemory / 1024 / 1024 / 1024);
+    }
+
+}
