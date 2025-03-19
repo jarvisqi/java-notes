e5b0a03e2131c1ac53943217d3db7f4a59a92602
jarvisqi <qdw8911@foxmail.com>
2025-03-19 16:15:34 +0800

    广度优先搜索（BFS）的算法

--- notes-basic/src/main/java/com/softmax/leet/MinPathLength.java
+++ notes-basic/src/main/java/com/softmax/leet/MinPathLength.java
@@ -26,11 +26,20 @@ public class MinPathLength {
         boolean[][] visited = new boolean[row][col];
         Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{0, 0});
+
+        // 外层 while 循环：当队列不为空时，继续搜索。
+        //size：记录当前层的节点数量。每次处理完一层后，路径长度 len 加 1。
         while (!queue.isEmpty()) {
             int size = queue.size(); //注意queue每次存放的是下一次可以遍历的相同距离的点
             len++; //queue不为空可以++
             // 每进入一层，路径长度加 1
             while (size-- > 0) {
+                //内层 while 循环：处理当前层的所有节点。
+                //从队列中取出一个节点 (i, j)，并标记为已访问。
+                //遍历四个方向，计算相邻节点的坐标 (ii, jj)。
+                //如果相邻节点越界、是障碍物或已经访问过，则跳过。
+                //如果相邻节点是目标点 (ti, tj)，则返回当前路径长度 len。
+                //否则，将相邻节点加入队列，等待下一层处理。
                 int[] temp = queue.poll();
                 int i = temp[0], j = temp[1];
                 visited[i][j] = true;
