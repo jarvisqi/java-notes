578d87024b01534eacef7453cac1f643a056a588
jarvisqi <qdw8911@foxmail.com>
2025-03-28 11:02:28 +0800

    rob

---
+++ notes-basic/src/main/java/com/softmax/leet/Rob.java
@@ -0,0 +1,26 @@
+package com.softmax.leet;
+
+/**
+ * 抢劫一排住户，但是不能抢邻近的住户，求最大抢劫量。
+ */
+public class Rob {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 1};
+        int[] arr2 = {2, 7, 9, 3, 1};
+        System.out.println(solution(arr1));
+        System.out.println(solution(arr2));
+    }
+
+    private static int solution(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        if (arr.length == 1) return arr[0];
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+        dp[1] = Math.max(dp[0], dp[1]);
+        for (int i = 2; i < arr.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i]);
+        }
+        return dp[arr.length - 1];
+    }
+}
